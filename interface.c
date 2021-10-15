f58ce4a06bf58f41237d5c4c042ecfbc5e296d1e
kristiandueholm <kristian618@gmail.com>
2021-10-15 19:15:29 +0200

    Implemented new print stack

--- interface.c
+++ interface.c
@@ -1,14 +1,29 @@
 #include "interface.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void print_stack(stack* s) {
-    node* p = s->head;
-    while (p->next != NULL)
-    {
-        printf("%d ", p->value);
-        p = p->next;
+void print_stack(stack *s) {
+    if (empty_stack(s)) {
+        printf("Stack is empty\n");
     }
-    printf("%d\n", p->value);
 
-    return;    
+    else {
+        int *stack_array = (int *)malloc(sizeof(int) * s->size);
+        node *p = s->head;
+
+        for (int i = 0; i < s->size; i++)
+        {
+            stack_array[i] = p->value;
+            p = p->next;
+        }
+
+        for (int i = s->size - 1; i >= 0; i--)
+        {
+            printf("%d ", stack_array[i]);
+        }
+        
+        printf("\n");
+    }
+
+    return;
 }

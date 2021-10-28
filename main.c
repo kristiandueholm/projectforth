bf680431231eb2789955966dac7e3295da60256d
kristiandueholm <kristian618@gmail.com>
2021-10-29 00:02:27 +0200

    Sandboxing

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include "stack.h"
 #include "interface.h"
+#include "arithmetic.h"
 #include <stdio.h>
 
 /* Sandboxing function */
 void main()
 {
-    printf("Hello World!");
+    stack *s = initialize_stack();
+    user_input(s);
+    print_stack(s);
+    user_input(s);
+    print_stack(s);
 }

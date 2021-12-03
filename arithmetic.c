a3b2148d931d61bd0d3d027268a8bc4110e81ebd
kristiandueholm <kristian618@gmail.com>
2021-12-03 10:29:58 +0100

    Arithmetic merge

--- arithmetic.c
+++ arithmetic.c
@@ -1,1 +1,40 @@
-// File for calculating arithmetic
+// File for calculating arithmetic operations
+
+#include "arithmetic.h"
+
+void plus(stack *s)
+{
+    assert(s->size > 1); // Stack size at least 2
+    int a = pop(s);
+    int b = pop (s);
+    push(s, a + b);
+    return;
+}
+
+// Contains issues calculating negatives
+void minus(stack *s)
+{
+    assert(s->size > 1);
+    int a = pop(s);
+    int b = pop(s);
+    push(s, a - b);
+    return;
+}
+
+void multiply(stack *s)
+{
+    assert(s->size > 1);
+    int a = pop(s);
+    int b = pop(s);
+    push(s, a - b);
+    return;
+}
+
+void divide(stack *s)
+{
+    assert(s->size > 1);
+    int a = pop(s);
+    int b = pop(s);
+    push(s, a - b);
+    return;
+}
--- arithmetic.h
+++ arithmetic.h
@@ -1,1 +1,10 @@
-// Header file for arithmetic
+#ifndef __ARITHMETIC_H_
+#define __ARITHMETIC_H_
+
+#include "stack.h"
+#include <assert.h>
+
+void plus(stack *s);
+void minus(stack *s);
+
+#endif // __ARITHMETIC_H_
